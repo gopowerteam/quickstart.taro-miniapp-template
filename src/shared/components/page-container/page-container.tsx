b6d602f43cad219d8e348eb82cb041c741f3b033
zhuchentong <2037630@163.com>
2021-05-26 11:55:15 +0800

    fixed page-container

--- src/shared/components/page-container/page-container.tsx
+++ src/shared/components/page-container/page-container.tsx
@@ -1,15 +1,7 @@
 import { AppStore } from '@/store/app.store'
 import { View } from '@tarojs/components'
 import React from 'react'
-import { useStore } from 'reto'
-import { AtActivityIndicator } from 'taro-ui'
 
 export const PageContainer = props => {
-    const appStore = useStore(AppStore)
-
-    return appStore.ready ? (
-        <View className="px-1">{props.children}</View>
-    ) : (
-        <AtActivityIndicator size={100} mode="center"></AtActivityIndicator>
-    )
+    return <View className="px-1">{props.children}</View>
 }
